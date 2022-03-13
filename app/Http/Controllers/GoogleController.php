6fcc5f14fb0e8bc15822f684cd8d18b81d036bb9
habibalihabsyi <habibalihabsyi123@gmail.com>
2022-03-13 15:14:51 +0700

    socialite

--- app/Http/Controllers/GoogleController.php
+++ app/Http/Controllers/GoogleController.php
@@ -17,17 +17,11 @@ class GoogleController extends Controller
     public function handleGoogleCallback()
     {
         try {
-      
             $user = Socialite::driver('google')->user();
-       
             $finduser = User::where('google_id', $user->id)->first();
-       
             if($finduser){
-       
                 Auth::login($finduser);
-      
                 return redirect()->intended('dashboard');
-       
             }else{
                 $newUser = User::create([
                     'name' => $user->name,
@@ -35,12 +29,9 @@ class GoogleController extends Controller
                     'google_id'=> $user->id,
                     'password' => encrypt('123456dummy')
                 ]);
-      
                 Auth::login($newUser);
-      
                 return redirect()->intended('dashboard');
             }
-      
         } catch (Exception $e) {
             dd($e->getMessage());
         }
